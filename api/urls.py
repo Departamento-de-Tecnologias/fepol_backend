4cd94df0405b0d1682f71d9f2dd3a1aa85e8fef2
Erick Lino <eri.sion@live.com>
2022-06-01 11:36:44 -0500

    Arreglo del Loggin

--- api/urls.py
+++ api/urls.py
@@ -5,20 +5,8 @@ from user.views import *
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    # path('', include(router.urls)),
-    # path('/', .as_view()),
-    #path('evento/', EventList.as_view()),
-    #path('evento/<int:pk>/', EventDetail.as_view()),
-    #path('eventoOrdenado/<str:order>/', EventDetail.as_view()),
-    #path('eventoTipo/<str:id_organization>/<str:event_type>/', EventDetail.as_view()),
-    #path('documento/', DocumentList.as_view()),
     path('', include('api.routers')),
-    #path('carrera/', CareerList.as_view()),
-    #path('organizacion/', OrganizationList.as_view()),
     path('organizacion/<str:pk>/miembros', AllMembersByOrgViewSet.as_view({'get': 'retrive'})),
-    #path('suborganizacion/', SubOrganizationList.as_view()),
-    #path('suborganizacion/<int:pk>/', SubOrganizationDetail.as_view()),
-    #path('suborganizacion/<str:id_organization>/', SubOrganizationDetail.as_view()),
     path('persona/', PersonList.as_view()),
     path('persona/manager/', PersonView.as_view()),
     path('persona/manager/<str:pk>/', PersonView.as_view()),
@@ -28,8 +16,5 @@ urlpatterns = [
     path('rol/', MemberRoleList.as_view()),
     path('auth/login/', AuthToken.as_view()),
     path('auth/login/web/', AuthCookie.as_view()),
-    #path('documento/', DocumentList.as_view()),
-    #path('documento/<str:pk>/', DocumentDetail.as_view()),
-    #path('documento/<str:doc_type>/', DocumentDetail.as_view())
 ]
 
